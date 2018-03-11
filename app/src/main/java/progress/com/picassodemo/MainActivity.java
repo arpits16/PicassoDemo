43f9a7959e25e77d6cda16d5ab44afce57d89c25
Arpit <arpits@mindfiresolutions.com>
2018-03-11 10:03:45 +0530

    Added Picasso Implementation

--- app/src/main/java/progress/com/picassodemo/MainActivity.java
+++ app/src/main/java/progress/com/picassodemo/MainActivity.java
@@ -2,12 +2,34 @@ package progress.com.picassodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    ImageView imageView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageview);
+        imageView1=findViewById(R.id.imageview1);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Picasso picassoReference=Picasso.get();
+       // picassoReference.setIndicatorsEnabled(true);
+
+        //Fetching image from URL
+        picassoReference.load("http://i.imgur.com/DvpvklR.png").resize(100,200).into(imageView);
+        //Fetching image from my resources
+        picassoReference.load(R.mipmap.ic_launcher_round).into(imageView1);
+
     }
 }
